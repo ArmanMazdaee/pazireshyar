61187b4cedbce52758664165e38d84e0bd3b8ac8
Arman Mazdaee <arman.maz1373@gmail.com>
2017-02-13 13:21:34 +0330

    Add login settings

--- pazireshyar/settings.py
+++ pazireshyar/settings.py
@@ -123,3 +123,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# Auth
+
+LOGIN_REDIRECT_URL = 'users:profile'
+LOGIN_URL = 'users:login'
+
