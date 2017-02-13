eef5e35bd1ef75b7920023799b10f17785c8eca3
Arman Mazdaee <arman.maz1373@gmail.com>
2017-02-13 11:41:46 +0330

    Add User login

--- users/urls.py
+++ users/urls.py
@@ -1,11 +1,21 @@
 from django.conf.urls import url
-from django.contrib.auth.views import login
+from django.contrib.auth import views as auth_views
+from django.views.generic import TemplateView
 
 from . import views
 
 app_name = 'users'
 
 urlpatterns = [
-    url('^signup/$', views.signup, name='signup'),
-    url('^login/$', views.login),
+    url(
+        r'^signup/$',
+        views.signup,
+        name='signup',
+    ),
+    url(
+        r'^login/$',
+        auth_views.login,
+        {'template_name': 'users/login.html'},
+        name='login',
+    ),
 ]
